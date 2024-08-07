bdfbf8fc7560956cd4d2cd7b680aa3132f9e4f28
Sophia Nesterenko <sofiia.nesterenko@nure.ua>
2024-08-07 20:48:03 +0200

    clean import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import TermsOfService from './components/Services/TermsOfService';
 import PrivacyPolicy from './components/Services/PrivacyPolicy';
